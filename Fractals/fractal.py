fd69e44c31988da34631988718de4cecc86187da
Squaess <babartosz.BNS@gmail.com>
2017-07-19 10:48:17 +0200

    Some initial work

---
+++ Fractals/fractal.py
@@ -0,0 +1,9 @@
+import turtle
+PROGNAME = 'Sierpinski Triangle'
+
+myPen = turtle.Turtle()
+myPen.ht()
+myPen.speed(5)
+myPen.pencolor('orange')
+
+points = [[-175,-125],[0,175],[175,-125]] #size of triangle
