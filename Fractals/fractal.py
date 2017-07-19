7f195591d0c3a6509b9f5883158bb9345555fb76
Squaess <babartosz.BNS@gmail.com>
2017-07-19 14:37:32 +0200

    Done sierpinski's triangle and carpet

--- Fractals/fractal.py
+++
@@ -1,9 +0,0 @@
-import turtle
-PROGNAME = 'Sierpinski Triangle'
-
-myPen = turtle.Turtle()
-myPen.ht()
-myPen.speed(5)
-myPen.pencolor('orange')
-
-points = [[-175,-125],[0,175],[175,-125]] #size of triangle
---
+++ Fractals/sierpinski_carpet.py
@@ -0,0 +1,57 @@
+import turtle
+PROGNAME = 'Sierpinski Carpet'
+SIZE = 150
+CENTER = [0,0]
+
+
+myPen = turtle.Turtle()
+myPen.speed(1000000000)
+myPen.color("#000000")
+myPen.penup()
+
+# This function change the size for creating new boxes
+def newSize(oldSize):
+    return oldSize/3
+
+# This function draws a box by drawing each side of the square and using the fill function
+# parameters are the center of box and length of edge
+def box(boxSize, middle):
+    myPen.goto(middle[0]-(boxSize/2), middle[1]-(boxSize/2))
+    myPen.pendown()
+    myPen.begin_fill()
+    # 0 deg.
+    myPen.forward(boxSize)
+    myPen.left(90)
+    # 90 deg.
+    myPen.forward(boxSize)
+    myPen.left(90)
+    # 180 deg.
+    myPen.forward(boxSize)
+    myPen.left(90)
+    # 270 deg.
+    myPen.forward(boxSize)
+    myPen.end_fill()
+    myPen.setheading(0)
+    myPen.penup()
+
+# function doing recursion for carpet
+def carpet(depth, middle, size):
+    box(size, middle)
+    if depth > 0:
+        # Drawing box under
+        carpet(depth-1, [middle[0],middle[1]-size], newSize(size))
+
+        #Drawing box above
+        carpet(depth-1, [middle[0],middle[1]+size], newSize(size))
+
+        #Drawing 3 boxes on the left
+        for i in range(3):
+            carpet(depth-1, [middle[0]-size, middle[1]-size + (i * size)], newSize(size))
+        #Drawing 3 boxes on the right
+        for i in range(3):
+            carpet(depth-1, [middle[0]+size, middle[1]-size + (i * size)], newSize(size))
+
+carpet(4, CENTER, SIZE)
+
+# This line couse the window not to close
+turtle.mainloop()
